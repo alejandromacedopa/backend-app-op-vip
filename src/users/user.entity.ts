960f9584b292e3a973e1cc10394e192df1fd1659
arlysanchez <arly.sanchez@upeu.edu.pe>
2024-11-15 12:53:52 -0500

    sericio de las direcciones

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,6 +1,7 @@
 import { hash } from "bcrypt";
+import { Address } from "src/address/address.entity";
 import { Rol } from "src/roles/rol.entity";
-import { BeforeInsert, Column, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+import { BeforeInsert, Column, Entity, JoinTable, ManyToMany, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
 
 @Entity({ name: 'users'})
@@ -48,6 +49,9 @@ export class User{
 })
   @ManyToMany(() => Rol, (rol) => rol.users)
     roles: Rol[];
+
+    @OneToMany(() => Address, address => address.id)
+    address: Address;
   
   @BeforeInsert()
   async hashPassword(){
